6b03bce3d9aac32ce0f3f0868fc3f2fbfafe4a3a
Guillermo <Guillermo@DESKTOP-OUUG4LG>
2022-12-26 10:28:14 +0000

    Version 2 creación de tabla desde consultas sql

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,5 +1,9 @@
-
 package kata5p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -7,13 +11,24 @@ package kata5p1;
  */
 public class Kata5P1 {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:C:\\Users\\Guillermo\\Desktop\\SQLiteDatabaseBrowserPortable\\Data\\Kata5.db";
+        String sql = "CREATE TABLE IF NOT EXISTS mail (\n"
+                + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+                + " Mail text NOT NULL);";
+        try (Connection conn = DriverManager.getConnection(url); Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         // TODO code application logic here
         SelectApp app = new SelectApp();
         app.selectAll();
+        createNewTable();
     }
-    
+
 }
